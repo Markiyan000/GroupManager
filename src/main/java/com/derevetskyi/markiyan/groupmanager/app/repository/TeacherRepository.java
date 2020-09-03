2c67663fc1979edff54901a0c622324c07be20ec
Markiyan Derevetskyi <markiyan07345@gmail.com>
2020-09-03 21:38:48 +0300

    changed query to DB for retrieving all not curators from JPQL to native SQL

--- src/main/java/com/derevetskyi/markiyan/groupmanager/app/repository/TeacherRepository.java
+++ src/main/java/com/derevetskyi/markiyan/groupmanager/app/repository/TeacherRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +16,7 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     @Query("select t from Teacher t left join fetch t.subjects where t.id = :id")
     Optional<Teacher> findById(@Param(value="id") Long id);
 
-    @Query("select t from Teacher t where t.id not in (select g.curator from Group g where g.curator is not null)")
+    //@Query("select t from Teacher t where t.id not in (select g.curator from Group g where g.curator is not null)")
+    @Query(value = "select * from teacher where id not in(select curator_id from group_ where curator_id is not null)", nativeQuery = true)
     List<Teacher> findAllNotCurators();
 }
